10ed63314d464dd1a80c1b5a33f29da0e201ca65
Abraham Atsiwo <ab30atsiwo@gmail.com>
2025-04-16 08:16:52 -0700

    updated files

--- app.py
+++ app.py
@@ -18,29 +18,52 @@ app = Dash(
 server = app.server 
 
 
-def main_layout(app: Dash) -> Dash:
-    app.layout = html.Div(
-        children=[
-            html.Script(src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-AMS-MML_SVG"),
-            html.Div(children=[navbar(app), mainbody(app)], className="container")
-        ],
-        className="main-container",
-        id="main_container",
-    )
-    return app
+# def main_layout(app: Dash) -> Dash:
+#     app.layout = html.Div(
+#         children=[
+#             html.Script(src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-AMS-MML_SVG"),
+#             html.Div(children=[navbar(app), mainbody(app)], className="container")
+#         ],
+#         className="main-container",
+#         id="main_container",
+#     )
+#     return app
 
 
-def main_callback(app):
-    navbar_callback(app)
+# def main_callback(app):
+#     navbar_callback(app)
 
 
-def main(app, port):
-    main_layout(app)
-    main_callback(app)
-    return app.run(debug=False, port=port)
+# def main(app, port):
+#     main_layout(app)
+#     main_callback(app)
+#     return app.run(debug=False, port=port)
+
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8050))
+#     # print(port)
+#     main(app=app, port=port)
+
+
+
+app.layout = html.Div(
+    children=[
+        html.Script(src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-AMS-MML_SVG"),
+        html.Div(children=[navbar(app), mainbody(app)], className="container")
+    ],
+    className="main-container",
+    id="main_container",
+)
+
+navbar_callback(app)
+
+
+
+
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    # print(port)
-    main(app=app, port=port)
+    app.run(port=port)
+    
